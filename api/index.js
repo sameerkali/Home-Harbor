91e19794e25332df168e5c0f64e5ad6d36e87618
Sameer Faridi <s.faridi007@gmail.com>
2023-10-11 11:45:03 +0530

    create signIn api route (kuchh samajh nhi aara 😢)

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.rout.js';
-import authRouter from './routes/auth.route.js';
-
+import userRouter from "./routes/user.rout.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -24,17 +23,15 @@ app.listen(port, () => {
   console.log(`Serving on ${port}`);
 });
 
-
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message,
+    message
   });
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/signin", signin);
 router.post("/signup", signup);
 
 export default router;
