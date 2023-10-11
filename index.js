1c6423e06a29b9b3d182732ba1b0b47d140f57a5
Sameer Faridi <s.faridi007@gmail.com>
2023-10-11 05:50:29 +0530

    connecting with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connection established with Mongoose");
+  })
+  .catch((e) => {
+    console.log(`Hacing a connection error ${e}`);
+  });
 
 const app = express();
 const port = 6969;
-
-
-
 
 app.listen(port, () => {
   console.log(`Serving on ${port}`);
