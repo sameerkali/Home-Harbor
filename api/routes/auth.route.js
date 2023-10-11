94523d37e8e778c3f38d26004864d0f79e7e01b0
Sameer Faridi <s.faridi007@gmail.com>
2023-10-11 11:45:05 +0530

    kuchh samajh nhi aara

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
+import { signup, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
