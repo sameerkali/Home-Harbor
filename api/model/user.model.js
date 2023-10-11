b4461b90e29cac23322eb314084447fe382e6a72
Sameer Faridi <s.faridi007@gmail.com>
2023-10-11 06:01:04 +0530

    create a user model and schema

---
+++ api/model/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        typeof: 'string',
+        required: true,
+        unique: true
+    },
+    email: {
+        typeof: 'string',
+        required: true,
+        unique: true
+    },
+    password: {
+        typeof: 'string',
+        required: true,
+    }
+})
+
+const User = mongoose.model('user', userSchema);
+export default User;
