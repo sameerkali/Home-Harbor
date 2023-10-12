8b5e1197d50f5560fbfbf513dc96f818464975b7
Sameer Faridi <s.faridi007@gmail.com>
2023-10-12 19:31:26 +0530

    update some errors

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500'>Home</span>
+            <span className='text-slate-700'>Harbour</span>
           </h1>
         </Link>
         <form
